55aa3627eb328a056464d7f994b6f27a4c7bf2de
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-08-21 22:07:49 +0800

    feat: folders of this project

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// Swiper JS
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
+  
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 5,
+  grabCursor: true,
+  slidesPerView: 'auto'
+});
+
+// Swiper JS
+
